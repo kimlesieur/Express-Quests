530b3dbf3870bc21258a75af81068b4d99ee66b7
Kim Lesieur <lesieurkim@gmail.com>
2022-11-26 15:31:39 +0100

    feat: add mysql2 package connection, add users paths

--- movieHandlers.js
+++ movieHandlers.js
@@ -1,3 +1,5 @@
+const database = require("./database");
+
 const movies = [
   {
     id: 1,
@@ -26,22 +28,71 @@ const movies = [
 ];
 
 const getMovies = (req, res) => {
-  res.json(movies);
+  database
+    .query("select * from movies")
+    .then(([movies]) => {
+      res.json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
 };
 
 const getMovieById = (req, res) => {
   const id = parseInt(req.params.id);
 
-  const movie = movies.find((movie) => movie.id === id);
-
-  if (movie != null) {
-    res.json(movie);
-  } else {
-    res.status(404).send("Not Found");
-  }
+  database
+    .query("select * from movies where id = ?", [id])
+    .then(([movies]) => {
+      if(movies[0]){
+        res.json(movies[0])
+      } else {
+        res.status(404).send("Not Found");
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("Server error");
+    });
 };
+
+const getUsers = (req, res) => {
+  database
+    .query("select * from users")
+    .then(([users]) => {
+      res.json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+
+const getUserById = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("select * from users where id = ?", [id])
+    .then(([users]) => {
+      if(users[0]){
+        res.json(users[0])
+      } else {
+        res.status(404).send("Not Found");
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("Server error");
+    });
+
+};
+
+
 
 module.exports = {
   getMovies,
   getMovieById,
+  getUsers,
+  getUserById
 };
